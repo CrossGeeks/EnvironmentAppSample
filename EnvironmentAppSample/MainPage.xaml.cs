5ed27588885c7365a44db4c59d0ac5ce4376b07e
Rendy  Del Rosario Campusano <rendy.delrosario@gmail.com>
2019-05-01 11:19:18 +0300

    sample added

---
+++ EnvironmentAppSample/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EnvironmentAppSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            #if DEV
+                    BackgroundColor = Color.LightGray;
+                    enviromentLabel.Text = "DEV";
+            #else
+                    BackgroundColor = Color.LightBlue;
+                    enviromentLabel.Text = "PROD";
+            #endif
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # EnvironmentAppSample
+
+Xamarin Forms Multiple Environment App Sample
+
+<p align="center">
+
+   <img src="https://media.giphy.com/media/jUzVxmE5EQEZLISn1o/giphy.gif" title="iOS"/>
+
+</p>
+
+Blog post: [Build applications based on environment configuration](http://www.xamboy.com/2019/05/01/build-applications-based-on-environment-configuration/
